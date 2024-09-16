bdedccc86b667ad974e268b70b00bafdb0e65e4b
MichaelVakula <94872285+upcae9cffb@users.noreply.github.com>
2024-09-16 11:21:31 +0200

    Add files via upload

---
+++ Singleton/src/main/java/org/example/Dvoucisli.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public final class Dvoucisli {
+    private final int a;
+    private final int b;
+
+    public  Dvoucisli(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public  int Scitani(){
+        int c = a + b;
+        return c;
+    }
+}
